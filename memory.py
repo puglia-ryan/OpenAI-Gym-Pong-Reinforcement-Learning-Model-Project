1c17a8d25da4a7e18c47a1f918ae9c56ff55a71b
Kachkéis Ice Cream <114811171+puglia-ryan@users.noreply.github.com>
2023-10-19 15:18:36 +0200

    Update memory.py
    Clean up code for meeting

--- memory.py
+++ memory.py
@@ -1,3 +1,4 @@
+#This file is responsible for creating the memory for the agent, since the agent will use multiple frames to analyse its moves
 from rl.memory import SequentialMemory
 
 def create_memory(limit=10000, window_length=4):
