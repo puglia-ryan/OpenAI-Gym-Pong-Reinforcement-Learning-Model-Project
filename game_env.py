921adb721b35d2b6c0c8f40f797025fe7db7e0ea
puglia-ryan <ryan.puglia.001@student.uni.lu>
2023-10-19 11:39:42 +0200

    Adding files to new repo

---
+++ game_env.py
@@ -0,0 +1,20 @@
+import gym
+import preprocess_frame as ppf
+
+
+class pong_game(object):
+
+    def __init__(self):
+        self.environment = gym.make("ALE/Pong-v5", render_mode="human")
+        self.environment.seed(0)
+
+    def reset(self):
+        self.frame = self.environment.reset()
+
+    def takeAction(self, action: int):
+        # In the Pong documentation, there are 6 actions: NOOP, FIRE, RIGHT, LEFT, RIGHTFIRE, LEFTFIRE
+        # We only need RIGHT and LEFT (so 2 and 3) for our model. The agent always returns 0 or 1 as a result, as there are only two output layers
+        # That's why we add 2 to the action
+        frame, reward, done, info = self.environment.step(action + 2)
+        processed_frame = ppf.resize_frame(frame)
+        return processed_frame, reward, done
