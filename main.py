8aa079587aeea13f4e11647d03d558fe0af9a905
puglia-ryan <ryan.puglia.001@student.uni.lu>
2023-12-13 16:44:24 +0100

    Removed the need for the game_env.py file. The environment is created in main.py and the interactions with the environment are done through the dqAgent instead of externally

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import gym
 import numpy as np
 
 import game_env
@@ -12,12 +13,10 @@ total_steps = 100000
 target_update_frequency = 1500
 step = 0
 
-pg = game_env.pong_game()
+pg = gym.make("ALE/Pong-v5")
 pg.reset()
 valid_actions = 2
 game_agent = agent.Agent((84, 84), valid_actions)
-cv2.namedWindow("Pong Game", cv2.WINDOW_NORMAL)
-
 game_agent.load_weights()
 
 
